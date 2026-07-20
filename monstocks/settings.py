a9c4a694e95aea748ea61a03f6f635c3b673676a
alowizdigital-create <alowizdigital@gmail.com>
2026-07-20 05:31:15 +0100

    Update settings.py

--- monstocks/settings.py
+++ monstocks/settings.py
@@ -107,10 +107,26 @@ WSGI_APPLICATION = 'monstocks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'zweeydb'),
+        'USER': os.getenv('DB_USER', 'zweeyuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Jeanpierre236'),
+        'HOST': os.getenv('DB_HOST', 'stocks-zweey-uhezrn'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
